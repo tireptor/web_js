279fcbbebf32982a72effb161eeb95161a1c157d
Vincent <vincent.lafargue96@gmail.com>
2020-05-03 18:39:02 +0200

    entrainement

---
+++ modification_attributs/exercie_divers.js
@@ -0,0 +1,7 @@
+var elementHtml = document.querySelector("#myP a");
+var queryAll    = document.querySelectorAll("#myP a");
+var child       = elementHtml.innerHTML;
+var childAll    = queryAll[0].innerHTML;
+//var child       = elementHtml.innerHTML;
+alert(child);
+alert(childAll);
